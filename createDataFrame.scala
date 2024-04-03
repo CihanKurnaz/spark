dd084f23344e99691051c059b8b9a2b93cb150b6
CihanKurnaz <78209988+CihanKurnaz@users.noreply.github.com>
2024-04-03 22:47:57 +0300

    Create createDataFrame.scala

---
+++ createDataFrame.scala
@@ -0,0 +1,10 @@
+val df = spark.read.json("examples/src/main/resources/people.json")
+
+df.show()
+// +----+-------+
+// | age|   name|
+// +----+-------+
+// |null|Michael|
+// |  30|   Andy|
+// |  19| Justin|
+// +----+-------+
